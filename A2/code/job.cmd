91af9506071b49e2dae5db6bc78e79b2eaefed2e
mahvili <mahyar.valizadeh@tum.de>
2013-11-22 00:27:12 +0100

    job command with exact name they needed

--- A2/code/job.cmd
+++ A2/code/job.cmd
@@ -22,10 +22,10 @@ module load metis
 mpiexec -n 8 ./gccg  ../dataFiles/cojack.geo.bin cojack_classic_proc3
 mpiexec -n 8 ./gccg  ../dataFiles/drall.geo.bin drall_classic_proc3
 mpiexec -n 8 ./gccg  ../dataFiles/pent.geo.bin pent_classic_proc3
-mpiexec -n 8 ./gccg  ../dataFiles/cojack.geo.bin cojack_dual_proc3 dual
-mpiexec -n 8 ./gccg  ../dataFiles/drall.geo.bin drall_dual_proc3 dual
-mpiexec -n 8 ./gccg  ../dataFiles/pent.geo.bin pent_dual_proc3 dual
-mpiexec -n 8 ./gccg  ../dataFiles/cojack.geo.bin cojack_nodal_proc3 nodal
-mpiexec -n 8 ./gccg  ../dataFiles/drall.geo.bin drall_nodal_proc3 nodal
-mpiexec -n 8 ./gccg  ../dataFiles/pent.geo.bin pent_nodal_proc3 nodal
+mpiexec -n 8 ./gccg  ../dataFiles/cojack.geo.bin cojack_mdual_proc3 dual
+mpiexec -n 8 ./gccg  ../dataFiles/drall.geo.bin drall_mdual_proc3 dual
+mpiexec -n 8 ./gccg  ../dataFiles/pent.geo.bin pent_mdual_proc3 dual
+mpiexec -n 8 ./gccg  ../dataFiles/cojack.geo.bin cojack_mnodal_proc3 nodal
+mpiexec -n 8 ./gccg  ../dataFiles/drall.geo.bin drall_mnodal_proc3 nodal
+mpiexec -n 8 ./gccg  ../dataFiles/pent.geo.bin pent_mnodal_proc3 nodal
 
