716c30a7f45905a37ecaa93c103d49f65b55b21e
Mahyar Valizadeh <mahyar.valizadeh@tum.de>
2013-12-18 20:31:57 +0100

    changes for supermuc

--- A2.2/code/job.cmd
+++ A2.2/code/job.cmd
@@ -3,14 +3,15 @@
 ## This script runs all the tests for the Firebenchmark, modifying the
 ## makefile to change between the optimization flags 
 ##
-#@ job_name = Team12
+#@ job_name = Team12_mytest_A2.3
 #@ job_type = parallel
-#@ class = fat
+#@ class = test
 #@ wall_clock_limit = 01:20:00
+#@ island_count = 1
 #@ node = 1
 #@ total_tasks = 8
 #@ node_usage = not_shared
-#@ initialdir = /home/hpc/h039v/di68goz/A2.1/code/
+#@ initialdir = /home/hpc/h039v/di68goz/A2.2/code
 #@ output = job$(jobid).out
 #@ error = job$(jobid).err
 #@ notification = always
@@ -19,13 +20,9 @@
 . /etc/profile
 . /etc/profile.d/modules.sh
 module load metis
-mpiexec -n 8 ./gccg  ../data/cojack.geo.bin cojack_classic_proc3
-mpiexec -n 8 ./gccg  ../data/drall.geo.bin drall_classic_proc3
-mpiexec -n 8 ./gccg  ../data/pent.geo.bin pent_classic_proc3
-mpiexec -n 8 ./gccg  ../data/cojack.geo.bin cojack_mdual_proc3 dual
-mpiexec -n 8 ./gccg  ../data/drall.geo.bin drall_mdual_proc3 dual
-mpiexec -n 8 ./gccg  ../data/pent.geo.bin pent_mdual_proc3 dual
-mpiexec -n 8 ./gccg  ../data/cojack.geo.bin cojack_mnodal_proc3 nodal
-mpiexec -n 8 ./gccg  ../data/drall.geo.bin drall_mnodal_proc3 nodal
-mpiexec -n 8 ./gccg  ../data/pent.geo.bin pent_mnodal_proc3 nodal
+module load papi
+mpiexec -n 8 ./gccg ../data/cojack.geo.bin drall_classic_8
+mpiexec -n 1 ./gccg ../data/cojack.geo.bin drall_classic_1
+mpiexec -n 8 ./gccg  ../data/cojack.geo.bin drall_metis_8 dual
+mpiexec -n 1 ./gccg  ../data/cojack.geo.bin drall_metis_1 dual
 
